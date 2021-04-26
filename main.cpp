0e33a22c1f965bd758b5432d90c44ea9a50ccff7
Marcelin <marcelinbelisle@gmail.com>
2021-04-26 15:42:06 -0400

    test

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ int main() {
  int* ptr_entier;
  // New sans delete : fuite de mémoire de 4 octets.
  ptr_flottant = new float;
- // Écriture utilisant un pointeur non-initialisé : comportement indéterminé.
- *ptr_entier = 5;
- // Lecture utilisant un pointeur non-initialisé : comportement indéterminé.
+ *ptr_entier = 4;
  std::cout << *ptr_entier << std::endl;
- // New sans delete : fuite de mémoire de 12 octets.
  int* ptr_tableau = new int[3];
 }
+//ujtrfjr
+
+///jtfj
